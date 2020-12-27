ccaa6837466f2d6b21e04c7e8c688822b8a4f9ad
David Berens <David.Berens@thyssenkrupp.com>
2020-12-27 17:50:23 +0100

    Added server side InjuryInput Validation

---
+++ middleware/validateEventInput.js
@@ -0,0 +1,100 @@
+const validateInjuryInput = (req, res, next) => {
+  console.log(req.body);
+  let errors = 0;
+  const validation = {
+    injuredValid1: 0,
+    injuredValid2: 0,
+    witnessValid1: 0,
+    mainAreaValid: 0,
+    subAreaValid: 0,
+    descriptionValid: 0,
+    spotsValid: 0,
+    typesValid: 0,
+  };
+
+  const { personalData, incidentData, injuryData } = req.body;
+  // Injured firstName
+  if (personalData[0].firstName.length === 0) {
+    errors++;
+    validation.injuredValid1 = -1;
+  } else {
+    validation.injuredValid1 = 1;
+  }
+
+  if (personalData[0].lastName.length === 0) {
+    errors++;
+    validation.injuredValid2 = -1;
+  } else {
+    validation.injuredValid2 = 1;
+  }
+
+  // Witness
+  if (
+    (personalData[1].firstName.length === 0 ||
+      personalData[1].lastName.length === 0) &&
+    !personalData[1].noWitness
+  ) {
+    errors++;
+    validation.witnessValid1 = -1;
+  } else {
+    validation.witnessValid1 = 1;
+  }
+
+  // MainArea
+  if (incidentData.masMainAreaId.length === 0) {
+    errors++;
+    validation.mainAreaValid = -1;
+  } else {
+    validation.mainAreaValid = 1;
+  }
+
+  // Subarea
+  if (incidentData.masSubAreaId.length === 0) {
+    errors++;
+    validation.subAreaValid = -1;
+  } else {
+    validation.subAreaValid = 1;
+  }
+
+  // Description
+  if (incidentData.incidentDescription.length === 0) {
+    errors++;
+    validation.descriptionValid = -1;
+  } else {
+    validation.descriptionValid = 1;
+  }
+
+  // Description
+  if (incidentData.incidentDescription.length === 0) {
+    errors++;
+    validation.descriptionValid = -1;
+  } else {
+    validation.descriptionValid = 1;
+  }
+
+  // Spots
+  if (injuryData.spots.length === 0) {
+    errors++;
+    validation.spotsValid = -1;
+  } else {
+    validation.spotsValid = 1;
+  }
+
+  // Types
+  if (injuryData.types.length === 0) {
+    errors++;
+    validation.typesValid = -1;
+  } else {
+    validation.typesValid = 1;
+  }
+
+  if (errors === 0) {
+    next();
+  } else {
+    res.status(400).send(validation);
+  }
+};
+
+module.exports = {
+  validateInjuryInput,
+};
--- routes/inci.routes.js
+++ routes/inci.routes.js
@@ -1,7 +1,12 @@
 const controller = require('../controllers/inci.controller');
+const { validateInjuryInput } = require('../middleware/validateEventInput');
 
 module.exports = (app) => {
   app.get('/api/event/incidents', controller.findAllIncidentsDecrypted);
 
-  app.post('/api/event/incident', controller.addIncidentEntry);
+  app.post(
+    '/api/event/incident',
+    validateInjuryInput,
+    controller.addIncidentEntry
+  );
 };
